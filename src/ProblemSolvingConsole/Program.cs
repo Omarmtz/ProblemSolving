52a6c8a552434f566cc8e9a391b1d3c464529e5c
Omar Martinez <omar@p1stache.com>
2019-01-14 15:23:13 -0600

    Problem 1 [Solution 1]: Solving Longest Word In Dictionary
    This is the first solution to this problem using optimal solution.
    Trie Solution is still under development.

--- src/ProblemSolvingConsole/Program.cs
+++ src/ProblemSolvingConsole/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using ProblemSolvingConsole.LongestWordInDictionary;
 
 namespace ProblemSolvingConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var wListA = new List<string> { "able", "ale", "apple", "bale", "kangaroo" };
+            var wordA = "abppplee";
+
+            var wListB = new List<string> { "ale", "apple", "monkey", "plea" };
+            var wordB = "abpcplea";
+
+            System.Console.WriteLine (LongestWordInDictionaryProblem.GetLongestWord (wordA, wListA));
+            System.Console.WriteLine (LongestWordInDictionaryProblem.GetLongestWord (wordB, wListB));
         }
     }
 }
